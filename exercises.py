95972a41f915df1c9213eb096048e390a771969d
Johan Zetterqvist <johan.zetterqvist@ki.se>
2015-12-01 00:14:38 +0100

    Modified the test again to cover up the either q or z is the least abundant word (both are)

--- exercises.py
+++ exercises.py
@@ -483,7 +483,7 @@ Be all my sins remember'd."""
 
     # and now we pass the file name to the function which will get the stats
     (most_abundant, least_abundant) = character_statistics(file_name)
-    assert (most_abundant, least_abundant) == ('e', 'z')
+    assert (most_abundant, least_abundant) in [('e', 'q'), ('e', 'z')]
 
     # we remove the temporary file
     os.unlink(file_name)
